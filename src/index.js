8281a001a0def7f6b87ac8223b1b860daf626979
aniket30i <iamaniket.saha.09@protonmail.com>
2024-04-11 02:50:21 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./starRating";
-import { useState } from "react";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./starRating";
+// import { useState } from "react";
 
 // function Test() {
 //   const [movieRating, setMovieRating] = useState(0);
@@ -18,11 +18,11 @@ import { useState } from "react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={["Terrible", "Bad", "OK", "Good", "Amazing"]}
     />
-    <StarRating maxRating={5} color="red" className="test" defaultRating={3} />
+    <StarRating maxRating={5} color="red" className="test" defaultRating={3} /> */}
   </React.StrictMode>
 );
